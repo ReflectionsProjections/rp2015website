7b1af6eb377744d7b92f9ac803e28c47b3db02a5
weterpoo <pywoo2@illinois.edu>
2015-01-02 04:43:50 -0500

    Peter 1/1/15
    Peter 1/1/15

--- demo/app.js
+++ demo/app.js
@@ -31,15 +31,15 @@
 
       $scope.panes = [
         {
-          header: 'Pane 1',
+          header: 'Speaker 1',
           content: 'Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi.'
         },
         {
-          header: 'Pane 2',
+          header: 'Speaker 2',
           content: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.'
         },
         {
-          header: 'Pane 3',
+          header: 'Speaker 3',
           content: 'Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non.',
 
           subpanes: [
